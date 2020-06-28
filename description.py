bbdc0e9139318d8ef9b0305003a41a046bf17d75
Somnath <p.somnath2599@gmail.com>
2020-06-28 21:14:58 +0530

    added description.py

---
+++ description.py
@@ -0,0 +1,11 @@
+# Statistical Summary
+import pandas
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+
+# descriptions
+description = data.describe()
+print(description)
